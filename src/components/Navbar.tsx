637d33aff580a3a5ace25f792846e91b80f3ac58
David <d-rz@live.com>
2026-03-02 21:16:03 -0500

    chore: add app icon (icon.svg) for favicon support

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -15,15 +15,13 @@ const Navbar = () => {
 						height="36"
 					>
 						<defs>
-							<linearGradient
-								id="logoGradFill"
-								x1="0%"
-								y1="0%"
-								x2="100%"
-								y2="100%"
-							>
-								<stop offset="40%" stopColor="#135ffb" />
-								<stop offset="100%" stopColor="#22d3ee" />
+							<linearGradient id="logoGradFill" x1="0%" y1="0%" x2="100%" y2="100%">
+								<stop className="svg-brand-from" offset="0%" stop-color="#22d3dd" />
+								<stop className="svg-brand-to" offset="90%" stop-color="#135ffb" />
+							</linearGradient>
+							<linearGradient id="botLogoFill" x1="0%" y1="0%" x2="100%" y2="100%">
+								<stop className="svg-brand-from" offset="40%" stop-color="#22d3dd" />
+								<stop className="svg-brand-to" offset="100%" stop-color="#135ffb" />
 							</linearGradient>
 						</defs>
 						<path
@@ -31,7 +29,8 @@ const Navbar = () => {
 							d="m35.89 1.46h-11.1-23.32v21.88 11.09l7.78 7.17h22.2l-7.36-7.17v-12.05h11.8c6.1 0 10.81 6.03 10.81 12.13v0.04c0 6.1-4.71 11.57-10.81 11.57h-11.1-11.1-12.22v21.54l33.79 0.13c6.22 0 12.64-1.82 17.69-4.92 9.57-5.86 16.36-16.44 16.36-28.42v0.07c0-18.31-15.11-33.06-33.42-33.06z"
 						/>
 						<path
-							fill="#22d3ee"
+							className="svg-logo-accent"
+							fill="#135ffb"
 							d="m56.43 66.08l-0.91 0.54c-4.71 2.89-9.98 4.64-15.43 5.26l6.93 6.85h22.08c0-0.01-12.67-12.65-12.67-12.65z"
 						/>
 					</svg>
